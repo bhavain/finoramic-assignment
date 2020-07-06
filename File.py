91e369a552bed25033882afa799ba325a1632b2e
bhavain <36289756+bhavain@users.noreply.github.com>
2020-07-06 12:49:55 +0530

    Python Task

---
+++ File.py
@@ -0,0 +1,25 @@
+import json
+import subprocess
+import sys
+import pkg_resources
+
+with open('/projectDependencies.json') as f:
+  data = json.load(f)
+
+uninstalled_packages = []
+
+
+for dep in data["Dependencies"]:
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", dep.split('==')[0]], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError as e:
+        uninstalled_packages.append(dep.split('==')[0])
+    
+
+if len(uninstalled_packages) == 0:
+    print("success")
+else:
+    print("These are the uninstalled packages")
+    for packages in uninstalled_packages:
+        print(packages,sep='\n')
+
